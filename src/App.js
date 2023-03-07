6f42bccc12937b83bcad81eacc6245d66fa56934
MaahesCr <psm3017@mail.ru>
2023-03-07 18:57:07 +0500

    refactor: making requests from the server to a custom hook

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { usePosts } from './hooks/usePost';
 import axios from 'axios';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetch';
 
 function App() {
 
@@ -24,20 +25,15 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [isPostsLoading, setIsPostsLoading] = useState(false)
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  })
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
-  }
-
-  async function fetchPosts() {
-    setIsPostsLoading(true)
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostsLoading(false)
-    }, 1000)
   }
 
   const removePost = (post) => {
@@ -57,6 +53,10 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+
       {isPostsLoading
         ? <div style={{display:'flex', justifyContent:'center', marginTop:30}}><Loader/></div> 
         : <PostList remove={removePost} posts = {sortedAndSearchedPosts} title = 'Список 1'/> 
